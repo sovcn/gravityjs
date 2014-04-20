2bded2e4f5646452be927aa8ceae16fb76d410fa
= <ksmith@innobdesign.com>
2014-04-20 16:57:43 +0000

    Fixing Chronograph repo.

--- public/graphs/services/graphs.js
+++ public/graphs/services/graphs.js
@@ -3,15 +3,15 @@
 //Articles service used for articles REST endpoint
 angular.module('mean.graphs').factory('Graphs', ['$resource', function($resource) {
     return $resource('graphs/:graphId', {
-        graphId: '@_id'
-    }, {
-        update: {
-            method: 'PUT'
-        },
-        byUser: {
-        	url: '/graphs/user',
-        	method: 'GET',
-        	isArray: true
-        }
+	        graphId: '@_id'
+	    }, {
+	        update: {
+	            method: 'PUT'
+	        },
+	        byUser: {
+	        	url: '/graphs/user',
+	        	method: 'GET',
+	        	isArray: true
+	        }
     });
 }]);
